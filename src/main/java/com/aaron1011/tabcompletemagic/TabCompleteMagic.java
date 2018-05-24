880dbc16ec0718a73b8402de96baefec4a16d1e4
Aaron Hill <aa1ronham@gmail.com>
2018-05-24 18:04:07 -0400

    Change plugin name to make McStats work

--- src/main/java/com/aaron1011/tabcompletemagic/TabCompleteMagic.java
+++ src/main/java/com/aaron1011/tabcompletemagic/TabCompleteMagic.java
@@ -27,7 +27,7 @@ import java.util.Optional;
 
 import javax.inject.Inject;
 
-@Plugin(id = "tabcompletemagic", name = "Tab Complete Magic", version = "2.0.0",
+@Plugin(id = "tabcompletemagic", name = "TabCompleteMagic", version = "2.0.0",
         description = "A plugin to add extra magic to tab completion", url = "https://github.com/Aaron1011/TabCompleteMagic", authors = "Aaron1011")
 public class TabCompleteMagic {
 
@@ -35,7 +35,7 @@ public class TabCompleteMagic {
     private Path defaultConfig;
 
     @Inject
-    private Metrics metrics;
+    Metrics metrics;
 
     private CommentedConfigurationNode rootNode;
     private HoconConfigurationLoader loader;
